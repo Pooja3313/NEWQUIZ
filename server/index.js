e0b25a8a7d7ddc13bef66498b1d477eb14f408f7
Pooja3313 <21cp324@bvmengineering.ac.in>
2024-10-18 10:15:07 +0530

    p32

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
+const cors = require("cors");
 // const mongoose = require('mongoose');
 dotenv.config({ path: './config.env'});
 require('./db/Conn');
 const authRouter = require('./router/auth');
+
+
+const corsOptions = {
+    origin: "https://newquiz-4gt3fjzyu-pooja3313s-projects.vercel.app/",
+    methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+    credentials: true,
+  };
+  
+  app.use(cors(corsOptions))
 
 app.use(express.json());
 app.use("/api/authh", authRouter);
