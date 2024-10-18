c343142603c9b3150ebe7b572ef5aa2c8cc85c17
Pooja3313 <21cp324@bvmengineering.ac.in>
2024-10-18 11:07:08 +0530

    p34

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const authRouter = require('./router/auth');
 
 
 const corsOptions = {
-    origin: "https://newquiz-4gt3fjzyu-pooja3313s-projects.vercel.app/",
+    origin: "https://newquiz-4gt3fjzyu-pooja3313s-projects.vercel.app",
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true,
   };
