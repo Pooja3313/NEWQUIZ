65b20d14d69be5aa056c0893e616711bbe40e61e
Pooja3313 <21cp324@bvmengineering.ac.in>
2024-10-20 13:45:51 +0530

    index.js1

--- server/index.js
+++ server/index.js
@@ -7,37 +7,14 @@ dotenv.config({ path: './config.env'});
 require('./db/Conn');
 const authRouter = require('./router/auth');
 
-const allowedOrigins = [
-  "https://newquiz-swart.vercel.app",
-  "https://newquiz-mkcywnwtd-pooja3313s-projects.vercel.app"
-];
 
 const corsOptions = {
-  origin: function (origin, callback) {
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-          // Allow requests if origin is in the allowed list or it's a non-origin request (like from Postman)
-          callback(null, true);
-      } else {
-          // Block requests from origins not in the list
-          callback(new Error('Not allowed by CORS'));
-      }
-  },
-  methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
-  credentials: true,  // Allow cookies or authentication headers
-};
-
-app.use(cors(corsOptions));
-
-
-
-
-// const corsOptions = {
-//     origin: "https://newquiz-swart.vercel.app",
-//     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
-//     credentials: true,
-//   };
+    origin: "https://newquiz-mkcywnwtd-pooja3313s-projects.vercel.app",
+    methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+    credentials: true,
+  };
   
-//   app.use(cors(corsOptions))
+  app.use(cors(corsOptions))
 
 app.use(express.json());
 app.use("/api/authh", authRouter);
